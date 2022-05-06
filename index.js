c765c7c4563c2e17f59054e271fb396553d5369b
RafiUlI <ctgrafi150@gmail.com>
2022-05-06 14:12:05 +0600

    nodemon added

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-app.use(cors());
-app.use(express.json());
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
 const uri = `mongodb+srv://${process.env.ASSIGNMENTELEVEN_USER}:${process.env.ASSIGNMENTELEVEN_PASSWORD}@cluster0.5tzt8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -22,7 +24,7 @@ async function run() {
 
     }
 }
-
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Hello world')
